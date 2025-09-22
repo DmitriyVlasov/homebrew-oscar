d94b37981a4e00ddfb477a6d472796f0ae560faa
Dima <dima@vlasovs.art>
2025-09-22 16:54:43 +0300

    Update Formula

--- Formula/oscar.rb
+++ Formula/oscar.rb
@@ -1,46 +1,60 @@
-# Формула для личного tap’а: например, putchastick/oscar/oscar.rb
 class Oscar < Formula
   desc "Open Source CPAP Analysis Reporter (OSCAR)"
   homepage "https://www.sleepfiles.com/OSCAR/"
   license "GPL-3.0-or-later"
 
-  # Стабильная сборка из официального репозитория разработчиков (GitLab)
   url "https://gitlab.com/CrimsonNape/OSCAR-code.git",
       using:  :git,
-      tag:    "v1.6.1"   # актуальная линия 1.6.x
+      tag:    "v1.6.1"
   head "https://gitlab.com/CrimsonNape/OSCAR-code.git", using: :git
 
-  # Qt5 требуется согласно пакетам Debian/FreeBSD для 1.5–1.6.x ветки
   depends_on "qt@5"
   depends_on "pkg-config" => :build
-
-  # zlib тянется из macOS; под Linux Homebrew подтянет свой пакет
   uses_from_macos "zlib"
 
-  def install
-    # Собираем в отдельном каталоге
-    mkdir "build" do
-      # Явно используем qmake из qt@5, чтобы избежать коллизий с системным qmake
-      system Formula["qt@5"].opt_bin/"qmake", "../OSCAR_QT.pro",
-             "CONFIG+=release",
-             "PREFIX=#{prefix}"
+  on_linux do
+    depends_on "gcc" => :build   # гарантируем наличие компилятора в Homebrew
+  end
 
-      # Сборка и установка
-      system "make"
-      system "make", "install"
+  def install
+    # qmake парсит вывод компилятора; принудим англоязычную локаль
+    ENV["LC_ALL"] = "C"
+    ENV["LANG"]   = "C"
+
+    # Определим, что использовать как CC/CXX
+    qmake_cc  = ENV.cc
+    qmake_cxx = ENV.cxx
+
+    if OS.linux?
+      # Используем именно brewed gcc, чтобы не зависеть от системы
+      gcc_formula = Formula["gcc"]
+      gcc_major   = gcc_formula.version.major
+      qmake_cc    = (gcc_formula.opt_bin/"gcc-#{gcc_major}").to_s
+      qmake_cxx   = (gcc_formula.opt_bin/"g++-#{gcc_major}").to_s
     end
 
-    # На всякий случай: если upstream не устанавливает desktop-файл/иконки сам,
-    # можно раскомментировать и подстроить пути (оставляю как подсказку).
-    # (share/"applications").install "resources/oscar.desktop" if File.exist?("resources/oscar.desktop")
-    # (share/"icons/hicolor/256x256/apps").install "resources/icons/oscar.png" if File.exist?("resources/icons/oscar.png")
+    mkdir "build" do
+      system Formula["qt@5"].opt_bin/"qmake",
+             "-spec", "linux-g++",
+             "QMAKE_CC=#{qmake_cc}",
+             "QMAKE_CXX=#{qmake_cxx}",
+             "QMAKE_LINK=#{qmake_cxx}",
+             "QMAKE_LRELEASE=#{Formula["qt@5"].opt_bin}/lrelease",
+             "CONFIG+=release",
+             "PREFIX=#{prefix}",
+             "../OSCAR_QT.pro"
+
+      system "make", "-j#{ENV.make_jobs}"
+
+      # У проекта нет цели install — ставим бинарник вручную
+      bin.install "oscar/OSCAR"
+      doc.install Dir["docs/*"], "README" if (buildpath/"docs").exist?
+    end
   end
 
   test do
-    # GUI мы не запускаем; проверяем, что бинарник установился
-    assert_predicate bin/"OSCAR", :exist?, "OSCAR binary must exist"
-    # Иногда приложение печатает помощь/версию; не критично, если код возврата != 0
-    out = shell_output("#{bin}/OSCAR --version 2>&1", 1)
-    assert_match(/OSCAR/i, out)
+    assert_predicate bin/"OSCAR", :exist?
+    output = shell_output("#{bin}/OSCAR --version 2>&1", 1)
+    assert_match(/OSCAR/i, output)
   end
 end
