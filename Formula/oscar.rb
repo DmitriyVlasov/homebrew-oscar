f059e2455fec61c5c47452aaea0792b7f4b20c28
Dima <dima@vlasovs.art>
2025-09-22 15:26:52 +0300

    Fixed Formula

--- Formula/oscar.rb
+++ Formula/oscar.rb
@@ -6,26 +6,35 @@ class Oscar < Formula
   license "GPL-3.0-or-later"
   head "https://gitlab.com/CrimsonNape/OSCAR-code.git", branch: "master"
 
-  depends_on "cmake" => :build
+  # qmake/Qt5 нужны и на сборку, и на рантайм
   depends_on "qt@5"
   depends_on "libzip"
   depends_on "zlib"
 
-def install
-  # убедимся, что qmake виден
-  ENV.prepend_path "PATH", Formula["qt@5"].opt_bin
+  # (опционально) удобно для обновлений
+  livecheck do
+    url :stable
+    strategy :git
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
 
-  # Генерим Makefile и собираем
-  system "qmake", "-config", "release"
-  system "make"
+  def install
+    # убедимся, что берем qmake из qt@5, а не системный
+    ENV.prepend_path "PATH", Formula["qt@5"].opt_bin
 
-  bin.install "OSCAR"
+    # генерим Makefile и собираем
+    system "qmake", "-config", "release"
+    system "make", "-j#{ENV.make_jobs}"
 
-  (share/"oscar").install Dir["icons/*"] rescue nil
-end
+    # ставим бинарь
+    bin.install "OSCAR"
 
+    # иконки — по желанию, если проект их ищет по относительным путям
+    (share/"oscar").install Dir["icons/*"] rescue nil
+  end
 
   test do
-    system "#{bin}/OSCAR", "--version"
+    # Проверим, что бинарь вообще запускается и выводит help
+    assert_match "Usage", shell_output("#{bin}/OSCAR --help 2>&1")
   end
 end
