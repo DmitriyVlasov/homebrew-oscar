6a0103e9276e9d14e50365fd5286b3c1567a9b6f
Dima <dima@vlasovs.art>
2025-09-22 11:54:09 +0300

    Added real sha-256 from brew fetch calculate

--- Formula/oscar.rb
+++ Formula/oscar.rb
@@ -2,7 +2,7 @@ class Oscar < Formula
   desc "Open Source CPAP Analysis Reporter"
   homepage "https://gitlab.com/CrimsonNape/OSCAR-code"
   url "https://gitlab.com/CrimsonNape/OSCAR-code/-/archive/v1.6.1/OSCAR-code-v1.6.1.tar.gz"
-  sha256 "deadbeef"
+  sha256 "0282f4e8347c3e52911be1809eae8832da64e961cae9c968278a0a6fbceb5d51"
   license "GPL-3.0-or-later"
   head "https://gitlab.com/CrimsonNape/OSCAR-code.git", branch: "master"
 
