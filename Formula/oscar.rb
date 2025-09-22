4984d2dcbfaf34ee9a2d20fa0b4af09b4bcd8469
Dima <dima@vlasovs.art>
2025-09-22 17:54:55 +0300

    Update Formula

--- Formula/oscar.rb
+++ Formula/oscar.rb
@@ -13,47 +13,58 @@ class Oscar < Formula
   uses_from_macos "zlib"
 
   on_linux do
-    depends_on "gcc" => :build   # гарантируем наличие компилятора в Homebrew
+    depends_on "llvm" => :build  # используем clang/clang++ из Homebrew
+    # При желании можно добавить: depends_on "make" => :build
   end
 
   def install
-    # qmake парсит вывод компилятора; принудим англоязычную локаль
+    # qmake парсит вывод компилятора — фиксируем англоязычную локаль
     ENV["LC_ALL"] = "C"
     ENV["LANG"]   = "C"
 
-    # Определим, что использовать как CC/CXX
-    qmake_cc  = ENV.cc
-    qmake_cxx = ENV.cxx
-
-    if OS.linux?
-      # Используем именно brewed gcc, чтобы не зависеть от системы
-      gcc_formula = Formula["gcc"]
-      gcc_major   = gcc_formula.version.major
-      qmake_cc    = (gcc_formula.opt_bin/"gcc-#{gcc_major}").to_s
-      qmake_cxx   = (gcc_formula.opt_bin/"g++-#{gcc_major}").to_s
+    # Выбираем компилятор: на Linux — clang++ из Homebrew LLVM
+    cxx = if OS.linux?
+      Formula["llvm"].opt_bin/"clang++"
+    else
+      ENV.cxx
     end
 
     mkdir "build" do
+      # Сборка через qmake (Qt5). Проект в корне называется OSCAR_QT.pro
       system Formula["qt@5"].opt_bin/"qmake",
-             "-spec", "linux-g++",
-             "QMAKE_CC=#{qmake_cc}",
-             "QMAKE_CXX=#{qmake_cxx}",
-             "QMAKE_LINK=#{qmake_cxx}",
+             # профиль для clang на Linux
+             "-spec", "linux-clang",
+             # Явно укажем инструменты и потоки
+             "QMAKE_CXX=#{cxx}",
+             "QMAKE_LINK=#{cxx}",
+             "QMAKE_CXXFLAGS+=-pthread",
+             "QMAKE_LFLAGS+=-pthread",
+             # lrelease из qt@5 (для перевода строк, если понадобится)
              "QMAKE_LRELEASE=#{Formula["qt@5"].opt_bin}/lrelease",
-             "CONFIG+=release",
+             # Релизная сборка
+             "CONFIG+=release thread",
+             # Префикс (не используется install-целью, но на будущее)
              "PREFIX=#{prefix}",
+             # сам проект
              "../OSCAR_QT.pro"
 
       system "make", "-j#{ENV.make_jobs}"
 
-      # У проекта нет цели install — ставим бинарник вручную
+      # У проекта нет цели "make install": ставим бинарник вручную
       bin.install "oscar/OSCAR"
+
+      # Документацию положим, если есть
       doc.install Dir["docs/*"], "README" if (buildpath/"docs").exist?
+
+      # При желании можно добавить desktop-файл/иконку, если появятся в репо:
+      # (share/"applications").install "resources/oscar.desktop" if File.exist?("resources/oscar.desktop")
+      # (share/"icons/hicolor/256x256/apps").install "resources/icons/oscar.png" if File.exist?("resources/icons/oscar.png")
     end
   end
 
   test do
     assert_predicate bin/"OSCAR", :exist?
+    # В GUI-приложениях код возврата может быть !=0; проверим хотя бы вывод
     output = shell_output("#{bin}/OSCAR --version 2>&1", 1)
     assert_match(/OSCAR/i, output)
   end
